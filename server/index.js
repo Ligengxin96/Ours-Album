2b33b681c8f7a06be9b93319f3bd36953f68faf2
Ligengxin <Ligengxin96@gmail.com>
2021-06-23 22:28:17 +0800

    fix bodyParser deprecation

--- server/index.js
+++ server/index.js
@@ -1,5 +1,4 @@
 import express from 'express';
-import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
@@ -14,8 +13,8 @@ const databaseConnectStr = process.env.CONNECTION_URL;
 const app = express();
 
 app.use(cors());
-app.use(bodyParser.json({ limit: '30mb', extended: true }));
-app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
+app.use(express.json({ limit: '30mb', extended: true }));
+app.use(express.urlencoded({ limit: '30mb', extended: true }));
 
 app.use('/v1/post', postRouter);
 
