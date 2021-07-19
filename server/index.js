e19d643440756faba4d27caf6edf7ec4cfa7604e
Ligengxin <Ligengxin96@gmail.com>
2021-07-19 23:27:03 +0800

    Update index.js

--- server/index.js
+++ server/index.js
@@ -24,7 +24,7 @@ app.use('/Ours-Album/v1/user', userRouter);
 
 mongoose.connect(databaseConnectStr, { 
   useNewUrlParser: true, 
-  useUnifiedTopology: true,
+  useUnifiedTopology: true
 }).then(() => {
   app.listen(port, () => {
     console.log(`${new Date()}: Server running on port: ${port}`);
